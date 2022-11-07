984f9a5d367e7fd08e5ef55558e70eef020e9d58
leylandpaladin <enwind123@gmail.com>
2022-11-07 19:26:16 +0300

    ALL DONE

--- pixelator.py
+++ pixelator.py
@@ -1,39 +1,70 @@
 import pygame as pg
 import cv2
-
+import numpy as np
 
 class ArtConverter:
-    def __init__(self, path='img/1.jpg', font_size=6):
+    def __init__(self, path='img/1.jpg', font_size=5, color_lvl=8):
         pg.init()
         self.path = path
-        self.image = self.get_image()
+        self.color_lvl = color_lvl
+        self.image, self.gray_image = self.get_image()
         self.RES = self.WIDTH, self.HEIGHT = self.image.shape[0], self.image.shape[1]
         print(self.RES)
         self.surface = pg.display.set_mode(self.RES)
         self.clock = pg.time.Clock()
 
-        self.ASCII_CHARS = '.",:;!~+~+-xmo*#W&8@'
+        self.ASCII_CHARS = 'ixzao*#MW&7%B@$'
         self.ASCII_COEFF = 255//(len(self.ASCII_CHARS) - 1)
         self.font = pg.font.SysFont('Courier', font_size, bold = True)
-        self.CHAR_STEP = int(font_size * 0.4)
-        self.RENDERED_ASCII_CHARS = [self.font.render(char,False,'white') for char in self.ASCII_CHARS]
-        print(str(self.RENDERED_ASCII_CHARS))
+        self.CHAR_STEP = int(font_size * 0.6)
+        self.PALETTE, self.COLOR_COEFF = self.create_palette()
+        
 
     def draw_converted_image(self):
 
-        char_indices = self.image // self.ASCII_COEFF
+        char_indices = self.gray_image // self.ASCII_COEFF
+        color_incides = self.image // self.COLOR_COEFF
         for x in range(0, self.WIDTH, self.CHAR_STEP):
             for y in range(0, self.HEIGHT, self.CHAR_STEP):
                 char_index = char_indices[x,y]
                 if char_index:
-                    self.surface.blit(self.RENDERED_ASCII_CHARS[char_index], (x,y))
+                    char = self.ASCII_CHARS[char_index]
+                    color  = tuple(color_incides[x,y])
+
+                    self.surface.blit(self.PALETTE[char][color], (x,y))
+
+    def create_palette(self):
+
+        colors, color_coeff = np.linspace(0,255,num=self.color_lvl, dtype=int, retstep=True)
+        color_palette = [np.array([r,g,b]) for r in colors for g in colors for b in colors]
+        palette = dict.fromkeys(self.ASCII_CHARS, None)
+        color_coeff = int(color_coeff)
+        for char in palette:
+            char_palette = {}
+            for color in color_palette:
+                color_key = tuple(color // color_coeff)
+                char_palette[color_key] = self.font.render(char, False, tuple(color))
+            palette[char] = char_palette
+
+        return palette, color_coeff
+
+
+    def save_image(self):
+
+        pygame_image = pg.surfarray.array3d(self.surface)
+        cv2_img = cv2.transpose(pygame_image)
+        final_image = cv2.cvtColor(cv2_img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite('converted_image.jpg', final_image)
+        print('SAVED')
+
 
     def get_image(self):
         self.cv2_image = cv2.imread(self.path)
         transposed_image = cv2.transpose(self.cv2_image)
+        image = cv2.cvtColor(transposed_image, cv2.COLOR_BGR2RGB)
         gray_image = cv2.cvtColor(transposed_image, cv2.COLOR_RGB2GRAY)
         
-        return gray_image
+        return image, gray_image
     
     def draw_cv2_image(self):
 
@@ -53,8 +84,10 @@ class ArtConverter:
         while True:
             for i in pg.event.get():
                 if i.type == pg.QUIT:
-
                     exit()
+                elif i.type == pg.KEYDOWN:
+                    if i.key == pg.K_s:
+                        self.save_image()
             self.draw()
             pg.display.set_caption(str(self.clock.get_fps()))
             pg.display.flip()
@@ -64,4 +97,3 @@ if __name__ == '__main__':
 
     app = ArtConverter()
     app.run()
-
